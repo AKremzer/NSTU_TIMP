f5387cd2fd252b9c0e820afb46d5230cb96eb3e1
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-02-21 21:03:26 +0700

    самописный split для задания

--- Практика1/Words.js
+++ Практика1/Words.js
@@ -1,3 +1,21 @@
-function hello() {
-    console.log("AAAAAAA");
+function splitString(inputString) {
+    let result = [], strLength = inputString.length, singleWord = "";
+    for (let i = 0; i < strLength; i++) {
+        if((inputString[i] != ' ') && (inputString[i].toLowerCase() == inputString[i].toUpperCase())) continue;
+        if(inputString[i] == ' ') {
+            if(singleWord == "") continue;
+            else {
+                result.push(singleWord);
+                singleWord = "";
+            }
+        }
+        else singleWord += inputString[i];
+    }
+    if(singleWord != "") result.push(singleWord);
+    console.log(result);
+    return result;
+}
+
+function orderString() {
+    splitString(document.getElementById("stringInput").value);
 }
