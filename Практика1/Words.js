5a6f473aeb6c2b36c270f5bab90764f5c6de1a57
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-02-21 21:07:30 +0700

    splitString без лишнего

--- Практика1/Words.js
+++ Практика1/Words.js
@@ -1,18 +1,14 @@
 function splitString(inputString) {
     let result = [], strLength = inputString.length, singleWord = "";
     for (let i = 0; i < strLength; i++) {
-        if((inputString[i] != ' ') && (inputString[i].toLowerCase() == inputString[i].toUpperCase())) continue;
-        if(inputString[i] == ' ') {
-            if(singleWord == "") continue;
-            else {
-                result.push(singleWord);
-                singleWord = "";
-            }
+        if ((inputString[i] != ' ') && (inputString[i].toLowerCase() == inputString[i].toUpperCase())) continue;
+        if ((inputString[i] == ' ') && (singleWord != "")) {
+            result.push(singleWord);
+            singleWord = "";
         }
         else singleWord += inputString[i];
     }
-    if(singleWord != "") result.push(singleWord);
-    console.log(result);
+    if (singleWord != "") result.push(singleWord);
     return result;
 }
 
