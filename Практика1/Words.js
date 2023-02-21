5944602db85fe10aaefc2af3c83d66d1de9f4c80
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-02-21 21:48:26 +0700

    поиск первого слова по алфавиту

--- Практика1/Words.js
+++ Практика1/Words.js
@@ -12,6 +12,27 @@ function splitString(inputString) {
     return result;
 }
 
+function findFirst(splitWords) {
+    let wordsCount = splitWords.length;
+    if(wordsCount == 0) return null;
+    let minWord = splitWords[0];
+    for (let i = 0; i < wordsCount; i++) {
+        if(splitWords[i] == minWord) continue;
+        let wordLength = splitWords[i].length < minWord.length ? splitWords[i].length : minWord.length;
+        for(let j = 0; j < wordLength; j++) {
+            if(splitWords[i][j].toLowerCase() < minWord[j].toLowerCase()) {
+                minWord = splitWords[i];
+                break;
+            }
+            else if(splitWords[i][j].toLowerCase() != minWord[j].toLowerCase()) break;
+        }
+        if ((splitWords[i].length < minWord.length) && (splitWords[i].toLowerCase() == minWord.substring(0, splitWords[i].length).toLowerCase())) 
+            minWord = splitWords[i];
+    }
+    return minWord;        
+}
+
 function orderString() {
-    splitString(document.getElementById("stringInput").value);
+    let splitWords = splitString(document.getElementById("stringInput").value);
+    console.log(findFirst(splitWords));
 }
