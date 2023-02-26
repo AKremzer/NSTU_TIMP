bebea3ab0469208912663499f2fc281de3f643df
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-02-26 13:20:26 +0700

    комментарии и небольшие изменения

--- Практика1/Checkers/Checkers.js
+++ Практика1/Checkers/Checkers.js
@@ -1,3 +1,4 @@
+// двумерный массив, отображающий текущее положение фигур на доске
 let boardArray = [
     [ null, 0, null, 1, null, 2, null, 3 ],
     [ 4, null, 5, null, 6, null, 7, null ],
@@ -7,18 +8,19 @@ let boardArray = [
     [ 12, null, 13, null, 14, null, 15, null ],
     [ null, 16, null, 17, null, 18, null, 19 ],
     [ 20, null, 21, null, 22, null, 23, null ]
-]
+]; 
+let piecesCounter = 0; 
 
-let piecesCounter = 0
-
-function getCell(row, cell) {
+// html для клетки доски в зависимости от ее положения на доске
+function getCell(row, cell) 
+{
     let cellColor = (row + cell) % 2 == 0 ? "white" : "black";
-    let checker = row < 4 && cellColor == "black" ? `<div class="checker black-piece" id="${piecesCounter}"></div>` : 
-                 (row > 5 && cellColor == "black" ? `<div class="checker white-piece" id="${piecesCounter}"></div>` : "");
-    if(checker != "") piecesCounter++;
+    let checker = row < 4 && cellColor == "black" ? `<div class="checker black-piece" id="${piecesCounter++}"></div>` : 
+                 (row > 5 && cellColor == "black" ? `<div class="checker white-piece" id="${piecesCounter++}"></div>` : "");
     return `<div id="cell-${row}-${cell}" class="cell ${cellColor}">${checker}</div>`
-}
+} 
 
+// заполнение игральной доски клетками и шашками (добавление дочерних элементов board)
 function buildBoard() {
     let result = "";
     for (let i = 1; i < boardArray.length + 1; i++) {
@@ -30,15 +32,17 @@ function buildBoard() {
     return result;
 }
 
+// ссылки на элементы DOM
 let boardCells = document.getElementsByClassName("cell");
 let whitePieces = document.getElementsByClassName("white-piece");
 let blackPieces = document.getElementsByClassName("black-piece");
 
 let turn = "white";
-let whiteScore = 12; // добавить условие выигрыша
+let whiteScore = 12; 
 let blackScore = 12;
-let playerPieces = whitePieces;
+let playerPieces = whitePieces; // доступные для взаимодействия шашки на текущем ходу
 
+// класс, описывающий конкретную шашку
 class Piece {
   constructor(id, index, king, moves, jump) {
     this.pieceId = id;
@@ -48,7 +52,8 @@ class Piece {
     this.hasJumps = jump;
   }
 }
- 
+
+// класс, описывающий варианты смещения шашки согласно ее цвету
 class Move {
   constructor(turn, move) {
     this.turn = turn;
@@ -57,13 +62,15 @@ class Move {
 }
 
 let selectedPiece = new Piece(-1, "", false, [], false);
-let piecesMoves = [];
-let disableNormalMoves = false;
+let piecesMoves = []; // доступные ходы для всех шашек текущего хода
+let disableNormalMoves = false; // true, если среди доступных ходов есть те, которые бьют шашку (обязательное взятие шашек - правило)
 
+// проверка выхода очередного хода за пределы доски
 function isInBorders(row, cell) {
     return row > -1 && row < 8 && cell > -1 && cell < 8;
 }
 
+// доступные для шашки ходы (без взятий шашек)
 function getPossibleMoves(piece) {
     let row = parseInt(piece.indexOfBoardPiece[5]) - 1, cell = parseInt(piece.indexOfBoardPiece[7]) - 1;
     let moves = [ new Move("white", [-1, -1]), new Move("white", [-1, 1]), new Move("black", [1, -1]), new Move("black", [1, 1]) ];
@@ -77,6 +84,7 @@ function getPossibleMoves(piece) {
     getPossibleJumps(piece); 
 }
 
+// доступные для шашки взятия
 function getPossibleJumps(piece) {
     let row = parseInt(piece.indexOfBoardPiece[5]) - 1, cell = parseInt(piece.indexOfBoardPiece[7]) - 1;
     let moves = [ new Move("white", [-2, -2]), new Move("white", [-2, 2]), new Move("black", [2, -2]), new Move("black", [2, 2]) ];
@@ -90,14 +98,17 @@ function getPossibleJumps(piece) {
                    result.push(`cell-${newRow + 1}-${newCell + 1}`);
             }
         }
+    // при наличии у шашки доступных взятий стандартные ходы недоступны
     if(result.length > 0) {
-        piece.hasJumps = true;
+        piece.hasJumps = true; 
         piece.possibleMoves = result;
     }
 }
 
+// добавление шашкам event listener'ов для взаимодействия с ними игрока
 function giveOnClicks(pieces) {
     disableNormalMoves = false;
+    // вычисление доступных ходов для всех шашек текущего хода
     for (let i = 0; i < pieces.length; i++) {
         let cell = document.getElementById(pieces[i].id).parentElement.id;
         let isKing = document.getElementById(pieces[i].id).classList.contains("king") ? true : false;
@@ -107,35 +118,45 @@ function giveOnClicks(pieces) {
         if(piece.hasJumps) disableNormalMoves = true;
         piecesMoves.push(piece);
     }
+    // если есть возможность взятия, взаимодействие будет возможно только с теми шашками, у которых есть эта возможность
     if (disableNormalMoves) {
         for (let i = 0; i < piecesMoves.length; i++)
             if(piecesMoves[i].hasJumps)
                 document.getElementById(piecesMoves[i].pieceId).addEventListener("click", getPlayerPieces);
     }
+    // иначе взаимодействовать можно с любой шашкой текущего хода
     else for (let i = 0; i < pieces.length; i++)
         pieces[i].addEventListener("click", getPlayerPieces);
 }
 
+// начало серии функций, вызываемых при нажатии на доступную шашку, определение шашек текушего хода
 function getPlayerPieces() {
     playerPieces = (turn == "white") ? whitePieces : blackPieces;
     removeCellOnClicks();
     resetBorders();
 }
 
+// удаление атрибутов onclick с клеток доски (которые были добавлены на предыдущем ходу для совершения хода на них)
 function removeCellOnClicks() {
     for (let i = 0; i < boardCells.length; i++) {
         boardCells[i].removeAttribute("onclick");
     }
 }
 
+// удаление с поля точек, обозначающих доступные ходы
+function removeDots() {
+    let dotChildren = document.getElementsByClassName("dot");
+    for (let i = 0; i < dotChildren.length; i++)
+        dotChildren[i].remove();
+}
+
+// удаление подсвечивания выбранной шашки
 function resetBorders() {
-    for (let i = 0; i < playerPieces.length; i++) {
-        playerPieces[i].style.outline = 0;
-    }
-    let dotChildren = Array.from(document.getElementsByClassName("dot"));
-    dotChildren.forEach(child => {
-    child.remove();
-    });
+    for (let i = 0; i < playerPieces.length; i++)
+        if(playerPieces[i].style.outline != 0) {
+            playerPieces[i].style.outline = 0;
+        }
+    removeDots();
     resetSelectedPieceProperties();
     getSelectedPiece();
 }
@@ -148,19 +169,12 @@ function resetSelectedPieceProperties() {
     selectedPiece.hasJumps = false;
 }
 
+// получение информации о выбранной шашке
 function getSelectedPiece() {
-    
     let id = parseInt(event.target.id);
     let cell = "";
     selectedPiece.pieceId = id;
-    for (let i = 0; i < 8; i++)
-        for (let j = 0; j < 8; j++) {
-            if (boardArray[i][j] == id) {
-                cell = `cell-${i + 1}-${j + 1}`;
-                break;
-            }
-        }
-    selectedPiece.indexOfBoardPiece = cell;
+    selectedPiece.indexOfBoardPiece = document.getElementById(selectedPiece.pieceId).parentElement.id;
     selectedPiece.isKing = document.getElementById(selectedPiece.pieceId).classList.contains("king") ? true : false;
     for (let i = 0; i < piecesMoves.length; i++)
         if(piecesMoves[i].pieceId == selectedPiece.pieceId) {
@@ -168,18 +182,20 @@ function getSelectedPiece() {
             selectedPiece.hasJumps = piecesMoves[i].hasJumps;
             break;
         }
-    enableCells()
+    enableCells();
 }
 
+// добавление атрибутов onclick клеткам, доступным для хода, для совершения хода на них
 function enableCells() {
     document.getElementById(selectedPiece.pieceId).style.outline = "3px solid yellow";
     for (let i = 0; i < selectedPiece.possibleMoves.length; i++) {
         let row = parseInt(selectedPiece.possibleMoves[i][5]) - 1, cell = parseInt(selectedPiece.possibleMoves[i][7]) - 1;
         boardCells[row * 8 + cell].innerHTML = `<div class="dot"></div>`;
-        boardCells[row * 8 + cell].setAttribute("onclick", `makeMove('cell-${row + 1}-${cell + 1}')`);
+        boardCells[row * 8 + cell].setAttribute("click", `makeMove('cell-${row + 1}-${cell + 1}')`);
     }
 }
 
+// ход (перемещение выбранной шашки и удаление съеденной)
 function makeMove(cellChosen) {
     document.getElementById(selectedPiece.pieceId).remove();
     let row = parseInt(selectedPiece.indexOfBoardPiece[5]) - 1, cell = parseInt(selectedPiece.indexOfBoardPiece[7]) - 1;
@@ -196,6 +212,7 @@ function makeMove(cellChosen) {
     modifyBoard(selectedPiece.indexOfBoardPiece, cellChosen, checkerEaten);
 }
 
+// модификация boardArray, присуждение очков и назначение шашки в дамки, если после модификации доски она оказалась в крайнем ряду
 function modifyBoard(selectedCell, nextCell, checkerEaten) {
     let row = parseInt(selectedCell[5]) - 1, cell = parseInt(selectedCell[7]) - 1;
     let rowTo = parseInt(nextCell[5]) - 1, cellTo = parseInt(nextCell[7]) - 1;
@@ -212,14 +229,13 @@ function modifyBoard(selectedCell, nextCell, checkerEaten) {
         if (turn == "white" && selectedPiece.pieceId > 11)
             blackScore--
     }
-    let dotChildren = document.getElementsByClassName("dot");
-    for (let i = 0; i < dotChildren.length; i++)
-        dotChildren[i].remove();
     if(!selectedPiece.hasJumps) resetSelectedPieceProperties();
+    removeDots();
     removeCellOnClicks();
     removePiecesOnClicks();
 }
 
+// удаление event listener'ов с шашек текущего хода
 function removePiecesOnClicks() {
     if (turn == "white")
         for (let i = 0; i < whitePieces.length; i++)
@@ -229,6 +245,7 @@ function removePiecesOnClicks() {
     changePlayer();
 }
 
+// функция, позволяющая шашке совершить еще одно взятие, если шашки стоят в ряд
 function settleJumpSeries() {
     selectedPiece.possibleMoves = [];
     selectedPiece.hasJumps = false;
@@ -240,10 +257,11 @@ function settleJumpSeries() {
                 playerPieces[i].addEventListener("click", getPlayerPieces);
         enableCells();
     }
-    else return true;
 }
 
+// смена игрока 
 function changePlayer() {
+    // проигрыш одной из сторон при потере всех шашек
     if((whiteScore == 0) || (blackScore == 0)) {
         document.getElementById("move").innerHTML = "Победа" + (whiteScore == 0 ? " черных" : " белых");
         return;
