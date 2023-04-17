37c1fc75d8efacd07231443a472b13320aca020a
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-04-17 16:35:31 +0700

    Добавлено изменение стиля

--- РГР/game-script.js
+++ РГР/game-script.js
@@ -1,14 +1,27 @@
-const user_input = document.getElementById('your-code')
+let user_input = document.getElementById('your-code')
+
 user_input.addEventListener('input', () => {
-        user_input.style.height = 0;
-        user_input.style.height = (user_input.scrollHeight + 1.5) + "px";
+    user_input.style.height = 0;
+    user_input.style.height = (user_input.scrollHeight + 1.5) + "px";
 })
 
 user_input.addEventListener('keypress', (e) => {
-        if(e.key === 'Enter') {
-                let lines = user_input.value.split(/\r\n|\r|\n/);
-                if (lines.length >= parseInt(user_input.rows)) {
-                        e.preventDefault();
-                }
+    if(e.key === 'Enter') {
+        let lines = user_input.value.split(/\r\n|\r|\n/);
+        if (lines.length >= parseInt(user_input.rows)) {
+            e.preventDefault();
         }
+    }
 })
+
+user_input.addEventListener('input', () => {
+    let elementName = document.getElementById("my-code-1").innerHTML.split(" ")[0];
+    let elementToChange = null;
+    if (elementName[0] == '#') {
+        elementName = elementName.substring(1);
+        elementToChange = document.getElementById(elementName);
+        elementToChange.style.cssText = user_input.value;
+        console.log(user_input.value);
+    }
+})
+
