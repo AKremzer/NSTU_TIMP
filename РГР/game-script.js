37ad9b39436b60db4616f1f6594e024790a43174
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-04-17 13:28:10 +0700

    Еще более токсичная textarea с ограничением числа строк

--- РГР/game-script.js
+++ РГР/game-script.js
@@ -1,5 +1,14 @@
-const textarea = document.getElementById('your-code')
-textarea.addEventListener('input', () => {
-    textarea.style.height = 0;
-    textarea.style.height = (textarea.scrollHeight - 4) + "px";
+const user_input = document.getElementById('your-code')
+user_input.addEventListener('input', () => {
+        user_input.style.height = 0;
+        user_input.style.height = (user_input.scrollHeight + 1.5) + "px";
+})
+
+user_input.addEventListener('keypress', (e) => {
+        if(e.key === 'Enter') {
+                let lines = user_input.value.split(/\r\n|\r|\n/);
+                if (lines.length >= parseInt(user_input.rows)) {
+                        e.preventDefault();
+                }
+        }
 })
