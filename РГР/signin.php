22b9bc878804ca6e15fbafce8a8c48957c8ceff1
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-05-09 20:42:53 +0700

    куки починили

--- РГР/signin.php
+++ РГР/signin.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $servername = "localhost";
 $username = "phplogin";
 $password = "phppass";
@@ -10,37 +12,53 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$login = $_POST['login'];
-$password = $_POST['password'];
+if (isset($_COOKIE['signtoken'])) {
+    $setquery = "SELECT * FROM users WHERE Login = ?";
+    $setstmt = $conn->prepare($setquery);
+    $setstmt->bind_param("s", $_COOKIE['signtoken']);
+    $setstmt->execute();
+    $setresult = $setstmt->get_result();
+    $row = $setresult->fetch_assoc();
+    $level = $row['Level'];
+    $params = "?login=" . urlencode($_COOKIE['signtoken']) . "&level=" . urlencode($level);
+    $redirectUrl = $level . "lev.html";
+    echo $redirectUrl . $params;
+    exit(); 
+}
 
-$query = "SELECT * FROM users WHERE Login = ?";
-$stmt = $conn->prepare($query);
-$stmt->bind_param("s", $login);
-$stmt->execute();
-$result = $stmt->get_result();
+elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $login = $_POST['login'];
+    $password = $_POST['password'];
 
-if ($result->num_rows == 1) {
-    $row = $result->fetch_assoc();
-    $hashedPassword = $row['Password'];
-    if (password_verify($password, $hashedPassword)) {
-        $cookie_name = "auth_token";
-        $cookie_value = $login;
-        setcookie($cookie_name, $cookie_value, time() + (86400 * 7), "/"); // куки на неделю
-        echo "Authentication successful. You are now logged in.";
-        $level = $row['Level'];
-        $redirectUrl = $level . "lev.html";
-        header("Location: " . $redirectUrl);
-    }
+    $query = "SELECT * FROM users WHERE Login = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $login);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $hashedPassword = $row['Password'];
+        if (password_verify($password, $hashedPassword)) {
+            setcookie('signtoken', $login, time() + 86400, '/');
+            echo "Authentication successful. You are now logged in.";
+            $level = $row['Level'];
+            $params = "?login=" . urlencode($login) . "&level=" . urlencode($level);
+            $redirectUrl = $level . "lev.html";
+            header("Location: " . $redirectUrl . $params);
+            exit();
+        }
+        else {
+            echo "Authentication failed. Invalid login or password.";
+            exit();
+        }
+    } 
     else {
         echo "Authentication failed. Invalid login or password.";
         exit();
     }
-} 
-else {
-    echo "Authentication failed. Invalid login or password.";
-    exit();
 }
+
 $stmt->close();
 $conn->close();
-exit();
 ?>
