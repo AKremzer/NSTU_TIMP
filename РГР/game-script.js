1117a485565b1869234356b62ab9a331b1737c3d
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-05-10 20:08:44 +0700

    за день (см. описание)
    добавлена проверка ответов, переход на новый уровень, начисление прогресса, блокировка кнопки проверки после прохождения уровня с целью предупреждения набивания прогресса одними и теми же правильными ответами, изменение содержимого страницы согласно данным из бд, загрузка при переходе страниц

--- РГР/game-script.js
+++ РГР/game-script.js
@@ -1,6 +1,13 @@
 let user_input = document.getElementById('your-code')
 let levelof20 = document.getElementById('levelof20')
 let userlog = document.getElementById('userlog')
+let userlevel = 0;
+
+let checkbut = document.getElementById("check");
+
+function removeList() {
+    checkbut.removeEventListener('click', checkfunc);
+}
 
 user_input.addEventListener('input', () => {
     user_input.style.height = 0;
@@ -26,22 +33,130 @@ user_input.addEventListener('input', () => {
     }
 })
 
+let checkfunc = function() {
+    {
+        let array = JSON.parse(answer);
+        let styles = window.getComputedStyle(divelem);
+        for (const [property, value] of array) {
+            if (removews(styles.getPropertyValue(property)) !== removews(value)) {
+                alert("Попробуйте еще раз!");
+                return;
+            }
+        }
+        let login = getCookieValue('signtoken');
+        $.ajax({
+            url: 'progress.php',
+            type: 'POST',
+            data: { winlogin: login },
+            success: async function(response) {
+                levelof20.innerHTML = response + "/20";
+                updateProgressBar(response);
+                userlevel = parseInt(response);
+                console.log("cool");
+                document.body.insertAdjacentHTML("afterbegin", "<div id=\"loading-overlay\">\n" +
+                    "    <p>Молодца круто классно</p>\n" +
+                    "</div>");
+                await sleep(2000);
+                document.body.classList.add('page-transition');
+                setTimeout(function() {
+                    window.location.href = response + "lev.html";
+                }, 500);
+            }
+        });
+    }
+}
+
+checkbut.addEventListener('click', checkfunc);
 function updateProgressBar(level) {
     let progress = (level / 20) * 100;
     var progressBarFill = document.getElementById("progress-bar-fill");
     progressBarFill.style.width = progress + "%";
+    progressBarFill.style.backgroundColor = "#f25f4c";
+    let page = window.location.pathname.split('/').pop();
+    if(parseInt(page[0]) < level)
+    {
+        removeList();
+        checkbut.style.backgroundColor = "#a7a9be";
+    }
 }
 
-let urlParams = new URLSearchParams(window.location.search);
-let level = urlParams.get('level');
-levelof20.innerHTML = level + "/20";
+function getCookieValue(cookieName) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(`${cookieName}=`)) {
+            return cookie.substring(cookieName.length + 1);
+        }
+    }
+    return null;
+}
 
-updateProgressBar(level);
-let user = urlParams.get('login');
-userlog.innerHTML = user;
-
-window.addEventListener('hashchange', () => {
-    var newLevel = urlParams.get('level');
-    updateProgressBar(newLevel);
+$(document).ready(function () {
+    const signtoken = getCookieValue('signtoken');
+    userlog.innerHTML = signtoken;
+    $.ajax({
+        url: 'progress.php',
+        type: 'POST',
+        data: { signtoken: signtoken },
+        success: function(response) {
+            levelof20.innerHTML = response + "/20";
+            updateProgressBar(response);
+            console.log(response);
+            userlevel = parseInt(response);
+        }
+    });
 });
 
+let signout = document.getElementById('signoutbut');
+signout.addEventListener('click', () => {
+    $.ajax({
+        url: 'signout.php',
+        type: 'GET',
+        success: function(response) {
+            if(response.includes("html")) {
+                window.location.href = response;
+            } else {
+                console.log(response);
+            }
+        }
+    });
+});
+
+let forward = document.getElementById('forward');
+let back = document.getElementById('back');
+
+forward.addEventListener('click', () => {
+    let page = window.location.pathname.split('/').pop();
+    let newlevel = parseInt(page[0]) + 1;
+    if (newlevel <= userlevel)
+        window.location.href = newlevel + "lev.html";
+})
+
+back.addEventListener('click', () => {
+    let page = window.location.pathname.split('/').pop();
+    let newlevel = parseInt(page[0]) - 1;
+    if (newlevel > 0)
+        window.location.href = newlevel + "lev.html";
+})
+
+let answer = "";
+let divelem = "";
+function getAnswer(level, elemid) {
+    divelem = document.getElementById(elemid);
+    $.ajax({
+        url: 'answers.php',
+        type: 'POST',
+        data: { level: level },
+        success: function(response) {
+            answer = response;
+        }
+    });
+}
+
+function removews(str) {
+    return str.replace(/\s/g, '');
+}
+
+function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
