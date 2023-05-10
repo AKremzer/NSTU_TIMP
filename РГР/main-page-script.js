b97e4569dad04796f191846de0a4266053e30c4f
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-05-10 20:12:11 +0700

    те самые легендарные скрипты в скриптах из прошлого коммита + куки работают

--- РГР/main-page-script.js
+++ РГР/main-page-script.js
@@ -1,3 +1,40 @@
+function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+function isCookieSet(cookieName) {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.indexOf(cookieName + '=') === 0) {
+            return true; // Cookie is set
+        }
+    }
+    return false; // Cookie is not set
+}
+$(document).ready(function () {
+    if(isCookieSet("signtoken")) {
+        $.ajax({
+            url: 'signin.php',
+            type: 'GET',
+            success: async function(response) {
+                if(response.includes("lev")) {
+                    document.body.insertAdjacentHTML("afterbegin", "<div id=\"loading-overlay\">\n" +
+                        "    <div class=\"loader\"></div>\n" +
+                        "    <p>Вход в систему...</p>\n" +
+                        "</div>");
+                    await sleep(2000);
+                    document.body.classList.add('page-transition');
+                    setTimeout(function() {
+                        window.location.href = response;
+                    }, 500);
+                    $('#loading-overlay').remove();
+                } else {
+                    console.log(response);
+                }
+            }
+        });
+    }
+});
 
 let log = document.getElementById("log");
 let reg = document.getElementById("reg");
