dee32521eac43ec5073663186aa03c94f6b6a093
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-05-13 19:22:23 +0700

    no comments

--- РГР/signup.php
+++ РГР/signup.php
@@ -9,10 +9,12 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+    echo "Connection error";
 }
 
 $newlogin = $_POST['newlogin'];
 $newpassword = $_POST['newpassword'];
+$newpassword = password_hash($newpassword, PASSWORD_DEFAULT);
 
 if (strlen($newlogin) <= 60) {
     $checkQuery = "SELECT * FROM users WHERE Login = ?";
@@ -26,17 +28,20 @@ if (strlen($newlogin) <= 60) {
         $insertStmt = $conn->prepare($insertQuery);
         $insertStmt->bind_param("ss", $newlogin, $newpassword);
         $insertStmt->execute();
-
-        echo "Values inserted successfully.";
+	    setcookie('signtoken', $newlogin, time() + 86400 * 7, '/');
+	    echo "1lev.html";
     } else {
-        echo "Login already exists.";
+        echo "Логин занят!";
+        exit();
     }
-
     $stmt->close();
     $insertStmt->close();
 } else {
-    echo "Login should be less than 60 characters.";
+    echo "Логин должен быть короче 60 символов!";
+    exit();
 }
 
 $conn->close();
+exit();
 ?>
+
